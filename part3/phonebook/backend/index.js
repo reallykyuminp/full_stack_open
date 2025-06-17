783f6556ae102a4f7e969991acd473c9f9271399
reallykyuminp <kyuminp@andrew.cmu.edu>
2025-06-17 12:02:18 -0400

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -34,12 +34,6 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
 let persons = [
   {
     id: "1",
